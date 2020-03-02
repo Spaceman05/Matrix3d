555d61250c9e10e4ed51d730516fef18f421c804
Felix <felix.black01@gmail.com>
2020-03-02 11:37:55 +0000

    Implement Slice functionality
    Extended __getitem__ to incorporate slicing

--- 3dMatrix.py
+++ 3dMatrix.py
@@ -68,7 +68,25 @@ class Matrix3:
         return self
         
     def __getitem__(self, indices):
-        return self.values[indices[0]][indices[1]][indices[2]]
+        if isinstance(indices[0], int):
+            irange = [indices[0]]
+        elif isinstance(indices[0], slice):
+            irange = range(indices[0].start if isinstance(indices[0].start, int) else 0, indices[0].stop if isinstance(indices[0].stop, int) else self.m)[::indices[0].step if isinstance(indices[0].step, int) else 1]
+
+        if isinstance(indices[1], int):
+            jrange = [indices[1]]
+        elif isinstance(indices[1], slice):
+            jrange = range(indices[1].start if isinstance(indices[1].start, int) else 0, indices[1].stop if isinstance(indices[1].stop, int) else self.n)[::indices[1].step if isinstance(indices[1].step, int) else 1]
+
+        if isinstance(indices[2], int):
+            krange = [indices[2]]
+        elif isinstance(indices[2], slice):
+            krange = range(indices[2].start if isinstance(indices[2].start, int) else 0, indices[2].stop if isinstance(indices[2].stop, int) else self.o)[::indices[2].step if isinstance(indices[2].step, int) else 1]
+
+        if len(irange) == 1 and len(jrange) == 1 and len(krange) == 1:
+            return self.values[indices[0]][indices[1]][indices[2]]
+            
+        return [[[self.values[i][j][k] for k in krange] for j in jrange] for i in irange]
 
     def __setitem__(self, indices, value):
         self.values[indices[0]][indices[1]][indices[2]] = value
